51a994dc9b2e4886bb4017108d4daaeb97a321d2
Toratom <thomas.poyet99@gmail.com>
2024-03-12 16:08:24 +0100

    transformations matrix, lookat matrix and projection matrix

--- math/matrix.hpp
+++ math/matrix.hpp
@@ -11,6 +11,10 @@ class Matrix;
 template <typename T, size_t N>
 using Vector = Matrix<T, N, 1>;
 
+using Mat3 = Matrix<double, 3, 3>;
+using Mat4 = Matrix<double, 4, 4>;
+using Vec3 = Vector<double, 3>;
+using Vec4 = Vector<double, 4>;
 
 //Declaration of the general class and its specialization (vectors and square matrix)
 template <typename T, size_t N, size_t M>
@@ -42,6 +46,10 @@ public:
         }
         return (*this);
     }
+    Matrix<T, N, M>& operator-=(const Matrix<T, N, M>& rhs) {
+        (*this) += (T)(-1)*rhs;
+        return (*this);
+    }
     Matrix<T, N, M>& operator*=(T scalar) {
         for (size_t i = 0; i < N; i += 1) {
             for (size_t j = 0; j < M; j += 1) {
@@ -58,6 +66,15 @@ public:
         (*this) *= (T)(1) / scalar;
         return (*this);
     }
+    Matrix<T, M, N> transpose() {
+       Matrix<T, M, N> result;
+       for (size_t i = 0; i < M; i += 1) {
+            for (size_t j = 0; j < N; j += 1) {
+                result(i, j) = (*this)(j, i);
+            }
+       }
+       return result;
+    }
     T dot(const Matrix<T, N, M>& rhs) const {
         T result = 0;
         for (size_t i = 0; i < N; i += 1) {
@@ -66,6 +83,10 @@ public:
             } 
         }
         return result;
+    }
+    T norm() const {
+        T result = (*this).dot(*this);
+        return sqrt(result);
     }
 
     //For col vectors only
@@ -121,6 +142,11 @@ Matrix<T, N, M> operator+(Matrix<T, N, M> lhs, const Matrix<T, N, M>& rhs) {
     lhs += rhs;
     return lhs;
 }
+template <typename T, size_t N, size_t M>
+Matrix<T, N, M> operator-(Matrix<T, N, M> lhs, const Matrix<T, N, M>& rhs) {
+    lhs -= rhs;
+    return lhs;
+}
 template <typename T, size_t N, size_t M, size_t K>
 Matrix<T, N, M> operator*(const Matrix<T, N, K>& lhs, const Matrix<T, K, M>& rhs) {
     Matrix<T, N, M> result;
@@ -153,18 +179,73 @@ Matrix<T, N, M> operator/(Matrix<T, N, M> lhs, T scalar) {
     lhs /= scalar;
     return lhs;
 }
+
+
+//Utils
 template <typename T>
-Matrix<T, 3, 3> rotation_matrix(const Vector<T, 3>& axis, T theta) {
+Matrix<T, 3, 3> rotation_matrix3(const Vector<T, 3>& axis, T theta) {
     T x = axis(0);
     T y = axis(1);
     T z = axis(2);
     T c = cos(theta);
     T s = sin(theta);
-    T one = (T)(1);
-    Matrix<T, 3, 3> result({(one - c)*x*x + c  , (one - c)*x*y - s*z, (one - c)*x*z - s*y,
-                            (one - c)*x*y + s*z, (one - c)*y*y + c  , (one - c)*y*z - s*x,
-                            (one - c)*x*z - s*y, (one - c)*y*z + s*z, (one - c)*z*z + c});
+    Matrix<T, 3, 3> result({(1 - c)*x*x + c  , (1 - c)*x*y - s*z, (1 - c)*x*z + s*y,
+                            (1 - c)*x*y + s*z, (1 - c)*y*y + c  , (1 - c)*y*z - s*x,
+                            (1 - c)*x*z - s*y, (1 - c)*y*z + s*x, (1 - c)*z*z + c});
     return result;
 }
+template <typename T>
+Matrix<T, 4, 4> rotation_matrix(const Vector<T, 3>& axis, T theta) {
+    Matrix<T, 3, 3> tmp = rotation_matrix3(axis, theta);
+    Matrix<T, 4, 4> result({tmp(0,0), tmp(0,1), tmp(0,2), 0,
+                            tmp(1,0), tmp(1,1), tmp(1,2), 0,
+                            tmp(2,0), tmp(2,1), tmp(2,2), 0,
+                            0       , 0       , 0       , 1});
+    return result;
+}
+template <typename T>
+Matrix<T, 4, 4> translation_matrix(const Vector<T, 3>& target) {
+    Matrix<T, 4, 4> result({1, 0, 0, target(0),
+                            0, 1, 0, target(1),
+                            0, 0, 1, target(2),
+                            0, 0, 0, 1});
+    return result;
+}
+template <typename T>
+Matrix<T, 3, 3> scale_matrix3(T s) {
+    Matrix<T, 3, 3> result({s, 0, 0,
+                            0, s, 0,
+                            0, 0, s});
+    return result;
+}
+template <typename T>
+Matrix<T, 4, 4> scale_matrix(T s) {
+    Matrix<T, 4, 4> result({s, 0, 0, 0,
+                            0, s, 0, 0,
+                            0, 0, s, 0,
+                            0, 0, 0, 1});
+    return result;
+}
+template <typename T>
+Matrix<T, 4, 4> look_at_matrix(const Vector<T, 3>& position, const Vector<T, 3>& target, const Vector<T, 3>& up) {
+    Vector<T, 3> Z = position - target;
+    Z /= Z.norm();
+    Vector<T, 3> X = up.cross(Z);
+    Vector<T, 3> Y = Z.cross(X);
+    Matrix<T, 4, 4> result({X(0), X(1), X(2), -X.dot(position),
+                            Y(0), Y(1), Y(2), -Y.dot(position),
+                            Z(0), Z(1), Z(2), -Z.dot(position),
+                            0   , 0   , 0   , 1});
+    return result;
+}
+template <typename T>
+Matrix<T, 4, 4> projection_matrix(T left, T right, T bottom, T top, T near, T far) {
+    Matrix<T, 4, 4> result({2*near/(right - left), 0                    , (right + left)/(right - left), 0,
+                            0                    , 2*near/(top - bottom), (top + bottom)/(top - bottom), 0,
+                            0                    , 0                    , -(far + near)/(far - near)   , -2*far*near/(far - near),
+                            0                    , 0                    , -1                           , 0});
+    return result;
+}
+
 
 #endif
